38b86cfd2c7cdc9ad31aee1cfd1ca37062bd2a7d
Alex Dana <alex.dana@epitech.eu>
2018-03-11 14:18:45 +0100

    * new: index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <p>This is a test for github options.</p>
     <p>This is the first push to S3 using travis !</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
